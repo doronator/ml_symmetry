54f18cc6b23b2e080d28bbf4feceeac9f81bca8f
Doron Bergman <doronator@gmail.com>
2018-05-31 08:54:09 -0700

    multilayer NN training examples working

--- src/run.py
+++ src/run.py
@@ -1,17 +1,61 @@
 import numpy as np
-from sklearn.neural_network import BernoulliRBM
+from sklearn import datasets, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 
-from src.simple_constrained_rbm import ConstrainedBernoulliRBM
+from src.NN_sans_bias import ConstrainedMLPClassifier
+from src.utils import nudge_dataset, plot_sample
 
-X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
-model = BernoulliRBM(n_components=2)
-model.fit(X)
+digits = datasets.load_digits()
+X = np.asarray(digits.data, 'float32')
+X, Y = nudge_dataset(X, digits.target)
+X = (X - np.min(X, 0)) / (np.max(X, 0) + 0.0001)  # 0-1 scaling
+X = 2*X - 1 # [-1,1] scaling
 
-print(model.transform(X))
+# plot_sample(X[0,:])
+# plot_sample(-X[0,:])
+# exit()
 
-print("and now the new RBM...")
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
+                                                    test_size=0.2,
+                                                    random_state=0)
 
-model2 = ConstrainedBernoulliRBM(n_components=2, fit_intercepts=False)
-model2.fit(X)
+model1 = MLPClassifier(solver='lbfgs',
+                        alpha=1e-5,
+                        activation='tanh',
+                        hidden_layer_sizes=(20, 10),
+                        random_state=1)
 
-print(model2.transform(X))
+model2 = ConstrainedMLPClassifier(solver='lbfgs',
+                                  alpha=1e-5, activation='tanh',
+                                  hidden_layer_sizes=(20, 10),
+                                  random_state=1,
+                                  fit_intercepts=False)
+
+model1.fit(X_train, Y_train)
+model2.fit(X_train, Y_train)
+
+
+print("un-constrained NN model test results:\n{}\n".format(
+    metrics.classification_report(
+        Y_test,
+        model1.predict(X_test))))
+
+print("constrained NN model test results:\n{}\n".format(
+    metrics.classification_report(
+        Y_test,
+        model2.predict(X_test))))
+
+print("And now on the inverted set:")
+
+
+print("un-constrained NN model test results:\n{}\n".format(
+    metrics.classification_report(
+        Y_test,
+        model1.predict(-X_test))))
+
+print("constrained NN model test results:\n{}\n".format(
+    metrics.classification_report(
+        Y_test,
+        model2.predict(-X_test))))
+
---
+++ src/utils.py
@@ -0,0 +1,44 @@
+from scipy.ndimage import convolve
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def nudge_dataset(X, Y):
+    """
+    This produces a dataset 5 times bigger than the original one,
+    by moving the 8x8 images in X around by 1px to left, right, down, up
+    """
+    direction_vectors = [
+        [[0, 1, 0],
+         [0, 0, 0],
+         [0, 0, 0]],
+
+        [[0, 0, 0],
+         [1, 0, 0],
+         [0, 0, 0]],
+
+        [[0, 0, 0],
+         [0, 0, 1],
+         [0, 0, 0]],
+
+        [[0, 0, 0],
+         [0, 0, 0],
+         [0, 1, 0]]]
+
+    shift = lambda x, w: convolve(x.reshape((8, 8)), mode='constant',
+                                     weights=w).ravel()
+    X = np.concatenate([X] +
+                       [np.apply_along_axis(shift, 1, X, vector)
+                        for vector in direction_vectors])
+    Y = np.concatenate([Y for _ in range(5)], axis=0)
+    return X, Y
+
+
+def plot_sample(x):
+    plt.figure(figsize=(4.2, 4))
+    plt.imshow(x.reshape((8, 8)), cmap=plt.cm.gray_r,
+               interpolation='nearest')
+    plt.xticks(())
+    plt.yticks(())
+
+    plt.show()
